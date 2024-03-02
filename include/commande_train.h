958ae0acfe325f611d0b762f60a095173bf9496d
jdumezy <jules.dumezy@gmail.com>
2024-03-02 09:21:11 +0100

    Update command

--- include/commande_train.h
+++ include/commande_train.h
@@ -2,6 +2,10 @@
 #define TRAIN_XWAY_INCLUDE_COMMANDE_TRAIN_H_
 
 #include <stdlib.h>
+#include <semaphore.h>
+
+#define NB_TRAINS 3
+#define NB_SECTIONS 100
 
 #define CHECK(status, msg)  \
 	if (-1 == (status)) {     \
@@ -10,6 +14,17 @@
 	}
 
 typedef void * (*pf_t)(void *);
+
+typedef struct {
+  int speed[NB_TRAINS];
+  sem_t mutex;
+} TrainSpeed;
+
+typedef struct {
+  int sections[NB_SECTIONS];
+  sem_t sem_sec[NB_SECTIONS];
+  sem_t mutex;
+} TrackSections;
 
 int init_train(int train_id);
 
--- src/commande_train.c
+++ src/commande_train.c
@@ -1,23 +1,30 @@
+#include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/mman.h>
 
 #include "commande_train.h"
 
-// Initialise a train
-int init_train(int train_id) {
-  int section = get_section_id();
-  int next_section = get_next_section_id(train_id);
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-  if (lock_section(section) == -1) return -1;
+// Missing functions
+int get_section_id();
+int get_next_section_id();
+int lock_section(int section_id);
 
-  printf("Initialisation OK for train %d\n", train_id);
 
-  if (lock_section(next_section) == -1) return 1;
 
-  return 0;
+void signal_thread() {
+    pthread_mutex_lock(&mutex);
+    // Change the condition
+    // ...
+    pthread_cond_signal(&cond); // Wake up one waiting thread
+    // or pthread_cond_broadcast(&cond); // To wake up all waiting threads if applicable
+    pthread_mutex_unlock(&mutex);
 }
 
-void thread_train(int train_id) {
+void* thread_train(int train_id) {
   /* Déroulement du thread 
    * 1. Initialisation
    *    - Mise en place initiale du circuit avant démarragevérification
@@ -37,9 +44,15 @@ void thread_train(int train_id) {
   */
 
   while(1) {
-    pthread
+    pthread_mutex_lock(&mutex);
+    while (/* condition not met */0) {
+      pthread_cond_wait(&cond, &mutex);
+    }
+    pthread_mutex_unlock(&mutex);
 
   }
+
+  return NULL;
 }
 
 // Initialisation common to all trains in same thread
@@ -59,3 +72,40 @@ void thread_train(int train_id) {
  * - is_section_switch
  * - set_next_section_switch
 */
+
+
+
+
+
+
+// initialise a train
+int init_train(int train_id) {
+  int section = get_section_id();
+  int next_section = get_next_section_id();
+
+  if (lock_section(section) == -1) return -1;
+
+  printf("initialisation ok for train %d\n", train_id);
+
+  if (lock_section(next_section) == -1) return 1;
+
+  return 0;
+}
+
+void thread_control(TrainSpeed *train_speed, int id) {
+
+}
+
+void thread_uspeed(TrackSections *track_sections, int id) {
+
+}
+
+int main() {
+  for (int i = 0; i < NB_TRAINS; i++) {
+    CHECK(init_train(i), "init_train");
+  }
+
+  // Initialize shared memomry
+  // Launch thread
+  // Join threads
+}
